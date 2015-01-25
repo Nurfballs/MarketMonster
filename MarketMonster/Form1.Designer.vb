e229aa802fe90f2f2af577b638960930704f2500
Nurfballs <casey@nurfballs.com.au>
2015-01-25 20:05:34 +1100

    Initial commit

---
+++ MarketMonster/Form1.Designer.vb
@@ -0,0 +1,97 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtCharacter = New System.Windows.Forms.TextBox()
+        Me.txtRealm = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(219, 68)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = true
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = true
+        Me.Label1.Location = New System.Drawing.Point(12, 18)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(56, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Character:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = true
+        Me.Label2.Location = New System.Drawing.Point(12, 45)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(40, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Realm:"
+        '
+        'txtCharacter
+        '
+        Me.txtCharacter.Location = New System.Drawing.Point(74, 16)
+        Me.txtCharacter.Name = "txtCharacter"
+        Me.txtCharacter.Size = New System.Drawing.Size(220, 20)
+        Me.txtCharacter.TabIndex = 3
+        Me.txtCharacter.Text = "Googlefu"
+        '
+        'txtRealm
+        '
+        Me.txtRealm.Location = New System.Drawing.Point(74, 42)
+        Me.txtRealm.Name = "txtRealm"
+        Me.txtRealm.Size = New System.Drawing.Size(220, 20)
+        Me.txtRealm.TabIndex = 4
+        Me.txtRealm.Text = "Blackrock"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6!, 13!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(318, 369)
+        Me.Controls.Add(Me.txtRealm)
+        Me.Controls.Add(Me.txtCharacter)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(false)
+        Me.PerformLayout
+
+End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtCharacter As TextBox
+    Friend WithEvents txtRealm As TextBox
+End Class
