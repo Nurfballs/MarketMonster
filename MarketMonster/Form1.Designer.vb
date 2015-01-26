4dea050d26037854ec04986cd1cb4d6de474761c
Nurfballs <casey@nurfballs.com.au>
2015-01-26 19:29:24 +1100

    Added auction filtering

--- MarketMonster/Form1.Designer.vb
+++ MarketMonster/Form1.Designer.vb
@@ -22,18 +22,17 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.txtCharacter = New System.Windows.Forms.TextBox()
         Me.txtRealm = New System.Windows.Forms.TextBox()
         Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
-        Me.Button2 = New System.Windows.Forms.Button()
-        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
-        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
         Me.GridControl1 = New DevExpress.XtraGrid.GridControl()
         Me.GridView1 = New DevExpress.XtraGrid.Views.Grid.GridView()
-        CType(Me.DataGridView1,System.ComponentModel.ISupportInitialize).BeginInit
+        Me.FormAssistant1 = New DevExpress.XtraBars.FormAssistant()
+        Me.DefaultLookAndFeel1 = New DevExpress.LookAndFeel.DefaultLookAndFeel(Me.components)
         CType(Me.GridControl1,System.ComponentModel.ISupportInitialize).BeginInit
         CType(Me.GridView1,System.ComponentModel.ISupportInitialize).BeginInit
         Me.SuspendLayout
@@ -79,7 +78,7 @@ Partial Class Form1
         Me.txtRealm.Name = "txtRealm"
         Me.txtRealm.Size = New System.Drawing.Size(220, 20)
         Me.txtRealm.TabIndex = 4
-        Me.txtRealm.Text = "Blackrock"
+        Me.txtRealm.Text = "Thaurissan"
         '
         'ProgressBar1
         '
@@ -88,37 +87,12 @@ Partial Class Form1
         Me.ProgressBar1.Size = New System.Drawing.Size(309, 23)
         Me.ProgressBar1.TabIndex = 6
         '
-        'Button2
-        '
-        Me.Button2.Location = New System.Drawing.Point(348, 45)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(75, 23)
-        Me.Button2.TabIndex = 7
-        Me.Button2.Text = "GO!"
-        Me.Button2.UseVisualStyleBackColor = true
-        '
-        'RichTextBox1
-        '
-        Me.RichTextBox1.Location = New System.Drawing.Point(35, 128)
-        Me.RichTextBox1.Name = "RichTextBox1"
-        Me.RichTextBox1.Size = New System.Drawing.Size(270, 259)
-        Me.RichTextBox1.TabIndex = 8
-        Me.RichTextBox1.Text = ""
-        '
-        'DataGridView1
-        '
-        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Location = New System.Drawing.Point(348, 128)
-        Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.Size = New System.Drawing.Size(293, 118)
-        Me.DataGridView1.TabIndex = 9
-        '
         'GridControl1
         '
-        Me.GridControl1.Location = New System.Drawing.Point(348, 268)
+        Me.GridControl1.Location = New System.Drawing.Point(22, 117)
         Me.GridControl1.MainView = Me.GridView1
         Me.GridControl1.Name = "GridControl1"
-        Me.GridControl1.Size = New System.Drawing.Size(644, 268)
+        Me.GridControl1.Size = New System.Drawing.Size(986, 408)
         Me.GridControl1.TabIndex = 10
         Me.GridControl1.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.GridView1})
         '
@@ -127,15 +101,16 @@ Partial Class Form1
         Me.GridView1.GridControl = Me.GridControl1
         Me.GridView1.Name = "GridView1"
         '
+        'DefaultLookAndFeel1
+        '
+        Me.DefaultLookAndFeel1.LookAndFeel.SkinName = "Visual Studio 2013 Light"
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6!, 13!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(1040, 572)
         Me.Controls.Add(Me.GridControl1)
-        Me.Controls.Add(Me.DataGridView1)
-        Me.Controls.Add(Me.RichTextBox1)
-        Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.ProgressBar1)
         Me.Controls.Add(Me.txtRealm)
         Me.Controls.Add(Me.txtCharacter)
@@ -144,7 +119,6 @@ Partial Class Form1
         Me.Controls.Add(Me.Button1)
         Me.Name = "Form1"
         Me.Text = "Form1"
-        CType(Me.DataGridView1,System.ComponentModel.ISupportInitialize).EndInit
         CType(Me.GridControl1,System.ComponentModel.ISupportInitialize).EndInit
         CType(Me.GridView1,System.ComponentModel.ISupportInitialize).EndInit
         Me.ResumeLayout(false)
@@ -158,9 +132,8 @@ End Sub
     Friend WithEvents txtCharacter As TextBox
     Friend WithEvents txtRealm As TextBox
     Friend WithEvents ProgressBar1 As ProgressBar
-    Friend WithEvents Button2 As Button
-    Friend WithEvents RichTextBox1 As RichTextBox
-    Friend WithEvents DataGridView1 As DataGridView
     Friend WithEvents GridControl1 As DevExpress.XtraGrid.GridControl
     Friend WithEvents GridView1 As DevExpress.XtraGrid.Views.Grid.GridView
+    Friend WithEvents FormAssistant1 As DevExpress.XtraBars.FormAssistant
+    Friend WithEvents DefaultLookAndFeel1 As DevExpress.LookAndFeel.DefaultLookAndFeel
 End Class
--- MarketMonster/Form1.vb
+++ MarketMonster/Form1.vb
@@ -6,6 +6,17 @@ Imports System.Data.OleDb
 
 
 Public Class Form1
+    Dim blnDownloadComplete As Boolean = True
+
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+
+    End Sub
+
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
@@ -27,15 +38,57 @@ Public Class Form1
         reader.Close()
         response.Close()
 
+        ' Download the AH data to file
+
+        Dim wc As WebClient = New WebClient
+        AddHandler wc.DownloadProgressChanged, AddressOf wc_ProgressChanged
+        AddHandler wc.DownloadFileCompleted, AddressOf wc_DownloadCompleted
+        wc.DownloadFileAsync(New Uri(result.files(0).url), "WoWAuctionData.json")
+        'wc.DownloadFileAsync(New Uri("http://www.wowuction.com/us/blackrock/horde/Tools/RealmDataExportGetFileStatic?type=csv&token=Oq_4EsX7G1zjuxD7Fits0w2"), "WoWAuctionData.csv")
+        Button1.Text = "Downloading"
+        Button1.Enabled = False
+
         ' Gather AH Data
         Dim AHDataURL As String = result.files(0).url
         'Dim AHDateModified As String = result.files(0).lastModified
 
-        request = WebRequest.Create(AHDataURL)
-        response = request.GetResponse()
-        dataStream = response.GetResponseStream()
-        reader = New StreamReader(dataStream)
-        responseFromServer = reader.ReadToEnd()
+
+        'ListBox1.Items.Add()
+
+
+
+        '        Dim items = JsonConvert.DeserializeObject(Of AuctionResult1())(json)
+        '        Dim mylist = items.ToArray
+        '        MessageBox.Show(items(0).auctions(0).auctions(0).item(0))
+
+
+
+    End Sub
+
+    Private Sub wc_ProgressChanged(ByVal sender As Object, ByVal e As DownloadProgressChangedEventArgs)
+        Dim bytesIn As Double = Double.Parse(e.BytesReceived.ToString())
+        Dim totalBytes As Double = Double.Parse(e.TotalBytesToReceive.ToString())
+        Dim percentage As Double = bytesIn / totalBytes * 100
+
+        ProgressBar1.Value = Int32.Parse(Math.Truncate(percentage).ToString())
+    End Sub
+
+    Private Sub wc_DownloadCompleted(ByVal sender As Object, ByVal e As System.ComponentModel.AsyncCompletedEventArgs)
+        blnDownloadComplete = True
+        Button1.Text = "Start Download"
+        Button1.Enabled = True
+        PopulateGrids()
+
+    End Sub
+
+    Public Sub PopulateGrids()
+
+
+        'request = WebRequest.Create(AHDataURL)
+        'response = request.GetResponse()
+        'dataStream = response.GetResponseStream()
+        Dim reader = New StreamReader(Application.StartupPath + "\WoWAuctionData.json")
+        Dim responseFromServer As String = reader.ReadToEnd()
 
         Dim dt As New DataTable
         dt.TableName = "AllAuctions"
@@ -62,7 +115,7 @@ Public Class Form1
 
                         End Select
                     Next
-                    RichTextBox1.Text = item.Value.ToString
+
             End Select
         Next
 
@@ -73,9 +126,9 @@ Public Class Form1
         Dim dt2 As DataTable = dt.DefaultView.ToTable
         dt2.TableName = "MyAuctions"
         ds.Tables.Add(dt2)
-        dt2.DefaultView.RowFilter = "owner = 'Chaosarrow'"
+        dt2.DefaultView.RowFilter = "owner = '" & txtCharacter.Text & "'"
 
-        DataGridView1.DataSource = ds.Tables("AllAuctions")
+        '        DataGridView1.DataSource = ds.Tables("AllAuctions")
         GridControl1.DataSource = ds.Tables("MyAuctions")
 
 
@@ -86,23 +139,7 @@ Public Class Form1
 
 
 
-
-        'ListBox1.Items.Add()
-
-
-
-        '        Dim items = JsonConvert.DeserializeObject(Of AuctionResult1())(json)
-        '        Dim mylist = items.ToArray
-        '        MessageBox.Show(items(0).auctions(0).auctions(0).item(0))
-
-
-
-
-
-
-
     End Sub
-
 
 End Class
 
